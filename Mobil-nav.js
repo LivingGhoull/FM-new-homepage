a48d4af0350b0cadb2c0a167bf8122222bfa17c9
Jesper Kjærgaard <jezper@hotmail.dk>
2023-11-27 16:10:22 +0100

    fast fix?

--- Mobil-nav.js
+++ Mobil-nav.js
@@ -1,10 +1,16 @@
 const navIcon = document.getElementById("mobil-icon");
 const mobilNav = document.getElementById("mobil-nav");
 
+console.log(navIcon);
+console.log(mobilNav);
+
 const imgClosed = "/icon-menu-close.svg"
 const imgOpen = "/icon-menu.svg"
 
 navIcon.onclick = () => {
+
+  console.log('clciked');
+  
   const currentImg = navIcon.src.split('/')[navIcon.src.split('/').length - 1]
   navIcon.src = `/${currentImg}` === imgOpen ? imgClosed : imgOpen;
 
